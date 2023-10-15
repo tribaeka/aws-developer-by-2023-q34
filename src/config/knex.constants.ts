8dd754567f089d9af9ebca2395b6f697c62b1d59
Yahor_Hlushak <Yahor_Hlushak@epam.com>
2023-10-15 15:36:15 +0300

    feat(rds): adopt code for remote

--- src/config/knex.constants.ts
+++ src/config/knex.constants.ts
@@ -1,13 +1,19 @@
 import { Knex } from 'knex';
+import * as fs from 'fs';
+import path from 'path';
 
+const caCertificate = fs.readFileSync(path.join(__dirname, 'rds-ca-2019-root.pem'));
 export const IMAGES_TABLE_NAME = 'images';
 export const KNEX_CONFIG: Knex.Config = {
   client: 'pg',
   connection: {
-    host: 'localhost',
+    host: 'yahor-hlushak-db.cithjq6js4st.us-east-2.rds.amazonaws.com',
     user: 'postgres',
     password: 'postgres',
     database: 'images',
-    port: 5432
+    port: 5432,
+    ssl: {
+      ca: caCertificate
+    }
   },
 }
