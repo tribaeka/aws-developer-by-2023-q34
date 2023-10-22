10edbf75ee5e8fe4c807173df8ed7030477acc8d
Yahor_Hlushak <Yahor_Hlushak@epam.com>
2023-10-22 10:10:41 +0300

    Revert "feat(rds): implement images api"
    This reverts commit ca72b05f62bbcd669ae524d10efa87abb34f6bf6.

--- src/config/aws.constants.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_REGION = 'us-east-2';
-// export const S3_IMAGES_BUCKET_NAME = 'yahor-hlushak-images-db';
-export const S3_IMAGES_BUCKET_NAME = 'yahor-hlushak-ci';
-export const S3_IMAGE_PREFIX = 'images/';
