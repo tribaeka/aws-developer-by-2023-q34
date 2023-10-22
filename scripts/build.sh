8f1568906b0de1c29237a47641eb6591cfe632ba
Yahor_Hlushak <Yahor_Hlushak@epam.com>
2023-10-22 11:50:36 +0300

    feat(sns-sqs): init

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,6 @@
 npx tsc
 
 cp package.json build/
+cp src/config/rds-ca-2019-root.pem build/config/
 
 node scripts/fixPackageJson.js
--- scripts/ec2-init.sh
+++ scripts/ec2-init.sh
@@ -2,9 +2,10 @@
 
 yum update -y
 yum install -y nodejs
+sudo yum install -y postgresql15
 
-aws s3 sync s3://yahor-hlushak-ci /opt/node-app
+aws s3 sync s3://yahor-hlushak-ci/app /opt/node-app
 
 cd /opt/node-app
 npm i
-node index.js
+node index.js > index.log.txt
